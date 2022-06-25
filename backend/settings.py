474ec2dbe3582eecc932765a879c9096a815d79e
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-06-25 15:40:27 +1200

    added poetry

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!-dp!_lyy03dvp9y528%g1+-z17_-(sgx6=z!o5ij&(%fn)s7&'
+SECRET_KEY = 'django-insecure-%@70&vvb7l3$&%)rv=gp88#aoc%c-p+ne(^lj)se#48^7igj$w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
